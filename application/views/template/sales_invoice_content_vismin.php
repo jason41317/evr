4090273486ba1dbc50deb46f56b1cae9a0d4fdb8
Joash Noble <noblejjoash@gmail.com>
2020-06-25 10:39:12 +0800

    Sales Invoice Print for VISMIN adjustment

--- application/views/template/sales_invoice_content_vismin.php
+++ application/views/template/sales_invoice_content_vismin.php
@@ -77,7 +77,7 @@
                     <td width="30%" class=" tbl-border-si" style="padding: 5px;"><?php echo $item->product_desc; ?></td>
                     <td width="10%" class="table-cellpadding tbl-border-si tbl-center"><?php echo number_format($item->inv_qty,0); ?></td>
                     <td width="10%" class="table-cellpadding tbl-border-si tbl-center"><?php echo $item->size; ?></td>
-                    <td width="20%" class="table-cellpadding tbl-border-si tbl-center"><span style="float: left;"><?php echo date('M-d',strtotime($item->exp_date)); ?></span>  <span style="float: right;"><?php echo number_format($item->inv_price,2); ?></span></td>
+                    <td width="20%" class="table-cellpadding tbl-border-si tbl-center"><span style="float: left;"><?php echo date('M-Y',strtotime($item->exp_date)); ?></span>  <span style="float: right;"><?php echo number_format($item->inv_price,2); ?></span></td>
                     <td width="30%" class="table-cellpadding tbl-border-si tbl-center"><?php echo number_format($item->inv_line_total_price,2); ?></td>
                 </tr>
                 <?php
