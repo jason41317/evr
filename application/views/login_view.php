3abff860bcd7a04b138874ef492a2947b6e275b1
Joash Noble <noblejjoash@gmail.com>
2020-06-30 21:02:25 +0800

    Login Autofocus

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -27,7 +27,7 @@
 
     </head>
 
-<body class="focused-form animated-content login-background">
+<body class="focused-form login-background">
         
         
 <div class="container" id="login-form">
@@ -53,7 +53,7 @@
 										<span class="input-group-addon">
 											<i class="ti ti-user"></i>
 										</span>
-										<input name="user_name" id="user_name" type="text" class="form-control" placeholder="Username" data-parsley-minlength="20" placeholder="At least 6 characters" required>
+										<input name="user_name" type="text" class="form-control" placeholder="Username" data-parsley-minlength="20" placeholder="At least 6 characters" required autofocus="autofocus">
 									</div>
 		                        </div>
 							</div>
@@ -120,10 +120,6 @@
     <script>
         $(document).ready(function(){
             var bindEventHandlers=(function(){
-
-		    	setTimeout(function(){	
-		    		$( "#user_name" ).focus();
-		    	},800);
 
                 $('#btn_login').click(function(){
 
