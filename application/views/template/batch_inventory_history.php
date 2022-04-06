8c22367d74b6c57f04b8383d456959628bf21885
Jason Patawaran <patawaranjason17@gmail.com>
2022-04-06 09:53:16 +0800

    Batch inventory history

---
+++ application/views/template/batch_inventory_history.php
@@ -0,0 +1,69 @@
+
+
+<div style="padding:1%;" >
+
+<center>
+    <table width="100%"  style="border-collapse: collapse;">
+        <thead>
+             <tr class="">
+                 <td style="border: 1px solid lightgrey;padding: 5px;"><b>Txn Date</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;"><b>Reference</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;"><b>Txn Type</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;"><b>Description</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;"><b>In</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;"><b>Out</b></td>
+                 <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;"><b>Balance</b></td>
+             </tr>
+
+        </thead>
+        <tbody>
+             <?php if(count($items)==0){ ?>
+                 <tr>
+                     <td colspan="8" style="border: 1px solid lightgrey;padding: 10px;" align="center">No transaction found.</td>
+                 </tr>
+             <?php } ?>
+
+             <?php foreach($items as $item){ ?>
+            <tr>
+                <td style="border: 1px solid lightgrey;padding: 5px;"><?php echo date("M d, Y",strtotime($item->txn_date)); ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;"><?php echo $item->ref_no; ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;"><?php echo $item->txn_type; ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;"><?php echo $item->description; ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;"><?php echo number_format($item->qty_in,2); ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;"><?php echo number_format($item->qty_out,2); ?></td>
+                <td style="border: 1px solid lightgrey;padding: 5px;text-align: right;font-weight: bolder;"><?php echo number_format($item->balance,2); ?></td>
+            </tr>
+             <?php } ?>
+
+        </tbody>
+    </table>
+    <br /><br />
+
+</center>
+
+
+</div>
+
+<style>
+tr {
+   border: none!important;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
