6f8dadc9bdd23d8ba30dd010366995d197a7fb99
Darren140315 <darrengbiteng.14@gmail.com>
2024-10-17 13:15:31 +0800

    Sales Summary with Sales Return
    Sales Summary with Sales Return

--- application/models/Sales_invoice_model.php
+++ application/models/Sales_invoice_model.php
@@ -332,7 +332,7 @@ class Sales_invoice_model extends CORE_Model
                     AND ai.is_deleted = FALSE AND ai.is_active = TRUE AND ai.is_approved = TRUE
                     GROUP BY ai.inv_no, aii.product_id , aii.adjust_price
                 
-                ) as returns ON returns.inv_no = si.sales_inv_no AND returns.product_id = sii.product_id AND returns.adjust_price = sii.inv_price
+                ) as returns ON returns.inv_no = si.sales_inv_no AND returns.product_id = sii.product_id
                 
                 WHERE si.date_invoice BETWEEN '$start' AND '$end' AND si.is_active=TRUE AND si.is_deleted=FALSE
 
@@ -345,6 +345,7 @@ class Sales_invoice_model extends CORE_Model
 
             ";
 
+            //var_dump($sql);
             return $this->db->query($sql)->result();
     }
 
